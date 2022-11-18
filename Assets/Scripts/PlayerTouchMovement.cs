1720fe4c1dec333eace8bae94b05902b016c0b60
Maryni <37656934+Maryni@users.noreply.github.com>
2022-11-18 22:53:46 +0200

    build 1.4 is succesfull

--- Assets/Scripts/PlayerTouchMovement.cs
+++ Assets/Scripts/PlayerTouchMovement.cs
@@ -45,29 +45,7 @@ public class PlayerTouchMovement : MonoBehaviour
         ETouch.Touch.onFingerDown += HandleFingerDown;
         EnhancedTouchSupport.Disable();
     }
-    
-    // private void OnGUI()
-    // {
-    //     GUIStyle labelStyle = new GUIStyle()
-    //     {
-    //         fontSize = 24,
-    //         normal = new GUIStyleState()
-    //         {
-    //             textColor = Color.white
-    //         }
-    //     };
-    //     if (movementFinger != null)
-    //     {
-    //         GUI.Label(new Rect(10, 35, 500, 20), $"Finger Start Position: {movementFinger.currentTouch.startScreenPosition}", labelStyle);
-    //         GUI.Label(new Rect(10, 65, 500, 20), $"Finger Current Position: {movementFinger.currentTouch.screenPosition}", labelStyle);
-    //     }
-    //     else
-    //     {
-    //         GUI.Label(new Rect(10, 35, 500, 20), "No Current Movement Touch", labelStyle);
-    //     }
-    //
-    //     GUI.Label(new Rect(10, 10, 500, 20), $"Screen Size ({Screen.width}, {Screen.height})", labelStyle);
-    // }
+
     #endregion Unity functions
 
     #region public functions
